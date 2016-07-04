000f747963c4180981662f5b47c20f90108a6c79
Jonathan Pastor <jonathancmoa@gmail.com>
2016-07-04 01:50:53 -0500

    Update reload_tidb.sh

--- reload_tidb.sh
+++ reload_tidb.sh
@@ -17,7 +17,7 @@ apt-get install -y mysql-client
 #sudo service mysql stop
 
 # Install Docker
-echo "deb http://get.docker.io/ubuntu docker main" > /etc/apt/sources.list.d/docker.list
+echo "deb http://get.docker.io/ubuntu docker main" >> /etc/apt/sources.list.d/docker.list
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 sudo apt-get update -y
 sudo apt-get install -y lxc-docker
