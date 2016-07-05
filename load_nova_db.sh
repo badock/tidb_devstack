3a96ba60c6e4ed0fdd07f7b4b71ec038daff4aa2
badock <jonathan.pastor@me.com>
2016-07-05 12:40:16 +0000

    save previous nova's sql state to prevent any loose of data;

--- load_nova_db.sh
+++ load_nova_db.sh
@@ -7,7 +7,14 @@ PASSWD="badock"
 mysql -h 127.0.0.1 -P $MYSQL_PORT -u root --password="$PASSWD" --execute="drop database nova;"
 mysql -h 127.0.0.1 -P $MYSQL_PORT -u root --password="$PASSWD" --execute="create database nova;"
 
+if [ ! -d save ]; then
+    mkdir -p save
+fi
+
+DATE=`date +%Y-%m-%d:%H:%M:%S`
+
 mysqldump -h 127.0.0.1 -P 3306 -u root  --password="admin" nova > nova.sql
+cp nova.sql save/nova_$DATE.sql
 
 mysql -h 127.0.0.1 -P $MYSQL_PORT -u root -D nova --password="$PASSWD" < nova.sql
 
